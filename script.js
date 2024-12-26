9ab35a651976c3e853db6e8fb20b90edab85b79e
tanuj96 <tanujmittal96@gmail.com>
2024-12-26 16:49:40 +0530

    Function Expression and Anonymous functions

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
 
 //1. Function Declaration or Function Defination or Function Statement
 
-function sqaure(num){
+// function sqaure(num){
+//     return num * num;
+// }
+
+
+//-------------------------------------------------------------------------------
+
+//2. Function Expression
+
+const square = function(num){  // here this function... is called anonymous function
     return num * num;
 }
