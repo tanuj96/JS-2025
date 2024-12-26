9df9f799862cad50627e470536fc24b3d537db61
tanuj96 <tanujmittal96@gmail.com>
2024-12-26 16:53:23 +0530

    First Class Functions

--- script.js
+++ script.js
@@ -13,6 +13,22 @@
 
 //2. Function Expression
 
-const square = function(num){  // here this function... is called anonymous function
+// const square = function(num){  // here this function... is called anonymous function
+//     return num * num;
+// }
+
+//-------------------------------------------------------------------------------
+
+//3. First Class Functions
+
+// -> functions can be used as variables like passed or returned from another function
+
+function sqaure(num){
     return num * num;
 }
+
+function displaySquare(fn){
+    console.log("Square is "+fn(5));
+}
+
+displaySquare(sqaure);
